48418b21e4d830d6e70ff1e7b4901b6b8478ed3a
Jason Ainsworth <jasoncruze@gmail.com>
2017-07-07 14:43:47 -0700

    add in CRUD functionality for admins

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,7 +9,29 @@ class ServicesController < ApplicationController
   end
 
   def new
+    @service = Service.new
+  end
 
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      flash[:notice] = "Service Sucessfully Added"
+      redirect_to service_path(@service)
+    else
+      flash[:alert] = "Something Went Wrong, Please Try Again"
+      render :new
+    end
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+    redirect_to services_path
+  end
+
+private
+  def service_params
+    params.require(:service).permit(:name, :description, :cost)
   end
 
 end
