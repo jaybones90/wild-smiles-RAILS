006c9b3551ad80563429d36fab9f16f489d77d84
Jason Ainsworth <jasoncruze@gmail.com>
2017-07-07 16:55:22 -0700

    update styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 Service.destroy_all
 Review.destroy_all
 
-50.times do |index|
+10.times do |index|
   Service.create!(name: Faker::App.name ,
                         description: Faker::Lorem.paragraph(4),
                         cost: Faker::Commerce.price)
 end
 
-250.times do |index|
+100.times do |index|
   Review.create!(author: Faker::GameOfThrones.character,
                         content: Faker::Lorem.paragraph(2),
-                        service_id: Faker::Number.between(1, 50))
+                        service_id: Faker::Number.between(1, 10))
 end
 
 p "Created #{Service.count} services"
